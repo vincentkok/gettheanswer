5271e3f3d9e5b43872ac81cee27a7ba271df978c
vincentkok <vincent@vincentkok.nl>
2010-11-15 21:01:01 +0530

    Some code formatting

--- src/main/java/nl/vincentkok/getanswers/applicationservice/impl/AskQuestionServiceImpl.java
+++ src/main/java/nl/vincentkok/getanswers/applicationservice/impl/AskQuestionServiceImpl.java
@@ -15,7 +15,6 @@ public class AskQuestionServiceImpl implements AskQuestionApplicationService {
 	public Question askQuestion(String questionToAsk) {
 		Question question = m_questionRepository.getObject(questionToAsk);
 				
-		
 		//Just application logic to determine whether there will be a new one.
 		//Asked for again is handled by the domain.
 		if (question == null) {
